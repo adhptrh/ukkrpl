cb62bce68874c9acfc40bd07c6f973e42f490cb5
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-02-03 19:40:07 +0700

    fix User to User_model

--- app/Controllers/IsiData.php
+++ app/Controllers/IsiData.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-use App\Models\User;
+use App\Models\User_model;
 use Config\Services;
 
 class IsiData extends BaseController
@@ -18,7 +18,7 @@ class IsiData extends BaseController
             return redirect()->to(base_url('/login'));
         }
 
-        $userdata = User::findByNIK(Services::session()->get("nik"));
+        $userdata = User_model::findByNIK(Services::session()->get("nik"));
         return view("isidata", ["userdata"=>$userdata]);
     }
 }
